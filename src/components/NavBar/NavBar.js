ce6192eb8e756e0ff24b5220acaad42a2635437d
AbdulRasheedNS <alrasheedtholicode@gmail.com>
2023-01-17 08:57:46 +0400

    logo cursur pointer added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import "./NavBar.css"
 function NavBar() {
   return (
     <div className='navbar'>
-      <a href="#">
+      <a href="/">
         <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="" />
       </a>
       <img className='avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" />
